121f41f42c642b895b03aa096fa340b99fda9a64
uzumaki-ak <anikydv38@gmail.com>
2025-12-26 16:33:25 +0530

    updated details and ui

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,23 +1,23 @@
-// import Metahead from "@/components/metahead";
-// import { navItems } from "@/lib/config/nav-confg";
-// import { Heading, Stack, Text } from "@chakra-ui/react";
+import Metahead from "@/components/metahead";
+import { navItems } from "@/lib/config/nav-confg";
+import { Heading, Stack, Text } from "@chakra-ui/react";
 
-// export default function BlogPage() {
-//   return (
-//     <>
-//       <Metahead
-//         title="Blog | College Portfolio"
-//         description="This is a portfolio website built using Next.js and Chakra UI by ChakraFramer Team for college students to showcase their resume, projects, and contact information."
-//         keywords={["Portfolio", "Resume", "ChakraFramer"]}
-//         url={navItems.blog}
-//       />
-//       <Stack gap={8}>
-//         <Heading as="h1" fontSize="3xl" mb={4} color={"peachpuff"}>
-//           Thoughts ↯
-//         </Heading>
+export default function BlogPage() {
+  return (
+    <>
+      {/* <Metahead
+        title="Blog | College Portfolio"
+        description="This is a portfolio website built using Next.js and Chakra UI by ChakraFramer Team for college students to showcase their resume, projects, and contact information."
+        keywords={["Portfolio", "Resume", "ChakraFramer"]}
+        url={navItems.blog}
+      /> */}
+      <Stack gap={8}>
+        <Heading as="h1" fontSize="3xl" mb={4} color={"peachpuff"}>
+          Thoughts ↯
+        </Heading>
 
-//         <Text color="whiteAlpha.600">Nothing to show...</Text>
-//       </Stack>
-//     </>
-//   );
-// }
+        <Text color="whiteAlpha.600">Nothing to show...</Text>
+      </Stack>
+    </>
+  );
+}
