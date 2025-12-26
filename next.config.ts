130a66bf739a7aa656e357459ddb4a69f923ceca
uzumaki-ak <anikydv38@gmail.com>
2025-12-26 16:38:44 +0530

    updated details and ui

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   reactStrictMode: true,
 
   eslint: {
-    // ⚠️ Completely disable ESLint during builds
     ignoreDuringBuilds: true,
   },
 };
